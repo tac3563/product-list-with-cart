40582370a972c66ef6e82e06ff06b5a1a9ecc0b6
tac3563 <tcox_bu@outlook.com>
2025-05-11 22:00:59 +0100

    [Update] hover states

--- src/components/AddToCart/AddToCart.tsx
+++ src/components/AddToCart/AddToCart.tsx
@@ -1,7 +1,5 @@
 import "./AddToCart.scss";
 import addToCartIcon from "/src/assets/images/icon-add-to-cart.svg";
-import incrementIcon from "/src/assets/images/icon-increment-quantity.svg"
-import decrementIcon from "/src/assets/images/icon-decrement-quantity.svg"
 import useStore from '../../stores/store.ts';
 
 type AddToCartProps = {
@@ -29,15 +27,33 @@ export default function AddToCart({thumbnail, id, price}: AddToCartProps) {
         {isOpen[id] &&
         <button key={`${id}-quantity-btn`} className='quantity-button'>
             <span>
-                <img key={`${id}-decrement-btn`} onClick={() => decrement(id)} src={decrementIcon} alt=""/>
+                <svg key={`${id}-decrement-btn`} onClick={() => decrement(id)} width="20" height="20"
+                     viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M10.0001 2.5C14.1251 2.5 17.5001 5.875 17.5001 10C17.5001 14.125 14.1251 17.5 10.0001 17.5C5.87512 17.5 2.50012 14.125 2.50012 10C2.50012 5.875 5.87512 2.5 10.0001 2.5ZM10.0001 1.25C5.18762 1.25 1.25012 5.1875 1.25012 10C1.25012 14.8125 5.18762 18.75 10.0001 18.75C14.8126 18.75 18.7501 14.8125 18.7501 10C18.7501 5.1875 14.8126 1.25 10.0001 1.25Z"
+                        fill="white"/>
+                      <circle className="fill-circle" cx="10" cy="10" r="7.5" fill="#C73B0F"/>
+                      <circle cx="10" cy="10" r="8.75" stroke="white" stroke-width="1.25" fill="#C73B0F"/>
+                    <path d="M5.00012 9.375H15.0001V10.625H5.00012V9.375Z" fill="white"/>
+                </svg>
+
             </span>
             {count}
             <span>
-                <img key={`${id}-increment-btn`}  src={incrementIcon} onClick={() => increment(id)} alt=""/>
+                <svg key={`${id}-increment-btn`} onClick={() => increment(id)} width="20" height="20"
+                     viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M10.0001 2.5C14.1251 2.5 17.5001 5.875 17.5001 10C17.5001 14.125 14.1251 17.5 10.0001 17.5C5.87512 17.5 2.50012 14.125 2.50012 10C2.50012 5.875 5.87512 2.5 10.0001 2.5ZM10.0001 1.25C5.18762 1.25 1.25012 5.1875 1.25012 10C1.25012 14.8125 5.18762 18.75 10.0001 18.75C14.8126 18.75 18.7501 14.8125 18.7501 10C18.7501 5.1875 14.8126 1.25 10.0001 1.25Z"
+                        fill="white"/>
+                      <circle className="fill-circle" cx="10" cy="10" r="7.5" fill="#C73B0F"/>
+                     <circle cx="10" cy="10" r="8.75" stroke="white" stroke-width="1.25" fill="#C73B0F"/>
+                    <path
+                        d="M15.0001 9.375H10.6251V5H9.37512V9.375H5.00012V10.625H9.37512V15H10.6251V10.625H15.0001V9.375Z"
+                        fill="white"/>
+                </svg>
             </span>
         </button>
         }
-
     </div>
   );
 }
--- src/components/Cart/CartList/CartList.tsx
+++ src/components/Cart/CartList/CartList.tsx
@@ -1,5 +1,4 @@
 import {parsePrice} from "../../../helpers/formatting.ts";
-import removeIcon from "../../../assets/images/icon-remove-item.svg";
 import emptyCartIcon from "../../../assets/images/illustration-empty-cart.svg";
 import './CartList.scss';
 import {AnimatePresence, motion} from 'framer-motion';
@@ -22,7 +21,6 @@ export default function CartList({orderStatus, cartItems, cartList, counters, re
             {cartItems.map((product) => (
                 <li className='cart-list-product' key={product}>
                     <div className="product-inner">
-
                         {orderStatus && (
                             <>
                                 <img className='product-thumbnail' src={cartList[product].thumbnail} alt=''/>
@@ -40,31 +38,33 @@ export default function CartList({orderStatus, cartItems, cartList, counters, re
                         )}
 
                         <AnimatePresence>
-
-                        {!orderStatus && (
-                            <motion.div
-                                initial={{opacity: 0}}
-                                animate={{opacity: 1}}
-                                exit={{opacity: 0}}
-                            >
-                                <div className="product-name">{product}</div>
-                                <div className="product-info">
-                                    <span className="product-info-qty">{counters[product]}x</span>
-                                    <span className="product-info-price">@{cartList[product].price}</span>
-                                    {!orderStatus && (
-                                        <span
-                                            className="product-info-total">${(parsePrice(cartList[product].price) * counters[product]).toFixed(2)}</span>
-                                    )}
-                                </div>
-                            </motion.div>
-                        )}
-                    </AnimatePresence>
-
+                            {!orderStatus && (
+                                <motion.div
+                                    initial={{opacity: 0, y: -15}}
+                                    animate={{opacity: 1, y: 0}}
+                                    exit={{opacity: 0, y: -15}}
+                                >
+                                    <div className="product-name">{product}</div>
+                                    <div className="product-info">
+                                        <span className="product-info-qty">{counters[product]}x</span>
+                                        <span className="product-info-price">@{cartList[product].price}</span>
+                                        {!orderStatus && (
+                                            <span
+                                                className="product-info-total">${(parsePrice(cartList[product].price) * counters[product]).toFixed(2)}</span>
+                                        )}
+                                    </div>
+                                </motion.div>
+                            )}
+                        </AnimatePresence>
                     </div>
 
                     {removeItem && (
-                        <span onClick={() => removeItem(product)}><img src={removeIcon}
-                                                                       alt="click to remove product fromc art"/></span>
+                        <span onClick={() => removeItem(product)}>
+                            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M10 1.25C5.125 1.25 1.25 5.125 1.25 10C1.25 14.875 5.125 18.75 10 18.75C14.875 18.75 18.75 14.875 18.75 10C18.75 5.125 14.875 1.25 10 1.25ZM10 17.5C5.875 17.5 2.5 14.125 2.5 10C2.5 5.875 5.875 2.5 10 2.5C14.125 2.5 17.5 5.875 17.5 10C17.5 14.125 14.125 17.5 10 17.5Z" fill="#AD8A85"/>
+                                <path d="M13.375 14.375L10 11L6.625 14.375L5.625 13.375L9 10L5.625 6.625L6.625 5.625L10 9L13.375 5.625L14.375 6.625L11 10L14.375 13.375L13.375 14.375Z" fill="#AD8A85"/>
+                            </svg>
+                        </span>
                     )}
                 </li>
             ))}
